d50a52d2e40ed188d15f581ef8df4e2b6cfd9859
hseirafi <hseirafi@gmail.com>
2014-11-26 12:17:46 +0000

    test for jobs and refactored

---
+++ models/Job.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+
+
+
+var jobSchema = mongoose.Schema({title:{type: String},description: {type: String}
+    
+});
+
+
+
+mongoose.model('Job', jobSchema);
+
+
+
+        
+
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 var express = require('express');
+var jobModel = require('./models/Job');
+var jobsData = require('./jobs-data.js');
+
 
 var app = express(); 
 
-app.set('views',__dirname)
+app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req,res){
+    jobsData.findJobs().then(function(collection){
+        res.send(collection);
+    })
+})
 app.get('*', function(req,res){
     res.render('index');
     
 }); 
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+jobsData.connectDB('mongodb://psdev:789456123@ds053130.mongolab.com:53130/jobfinder')
+.then(function(){
+    console.log('connected to mongodb successfully!')
+    jobModel.seedJobs();
+});
+
 
 app.listen(process.env.PORT, process.env.IP);
